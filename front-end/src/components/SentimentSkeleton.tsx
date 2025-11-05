143ac782b0ba94dc2020586e400eb8efc55602dd
JunhOpportunity <twinjyjh5@gmail.com>
2025-11-05 23:28:04 +0900

    feat($차트) : 스켈레톤 컴포넌트 구현

---
+++ front-end/src/components/SentimentSkeleton.tsx
@@ -0,0 +1,8 @@
+export default function SentimentSkeleton() {
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow-md animate-pulse">
+      <div className="h-6 bg-gray-300 rounded w-1/3 mx-auto mb-6"></div>
+      <div className="h-80 bg-gray-200 rounded-lg"></div>
+    </div>
+  );
+}
